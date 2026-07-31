b7cb5cc00c2bce7e4339009bb576e4c69e6fd1fc
Sachin Adhikari <sachinadhikari933@gmail.com>
2026-08-01 05:15:52 +0530

    Time: 2 ms (96.67%) | Memory: 47.2 MB (51.41%) - LeetSync

--- DSA-LeetCode/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ DSA-LeetCode/1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -1,24 +1,25 @@
 class Solution {
-    public static boolean help(int r , int c , int arr[][]){
-        for(int i =0 ; i < arr.length ; i++){
-            if(arr[i][c]==1 && i !=r)return false;
-        }
-        for(int i =0 ; i < arr[0].length ; i++){
-            if(arr[r][i]==1 && i !=c)return false;
-        }
-
-        return true;
-
-    }
+   
     public int numSpecial(int[][] arr) {
         int ans=0;
+        int row[]=new int[arr.length];
+        int col[]=new int[arr[0].length];
      for(int i =0 ;i < arr.length ; i++){
         for(int j =0 ; j < arr[0].length ; j++){
             if(arr[i][j]==1){
-                if(help(i,j,arr))ans++;
+                row[i]++;
+                col[j]++;
             }
      }
      }   
+
+
+        for(int i =0 ; i < arr.length ; i++){
+            for(int j =0 ; j  < arr[0].length ; j++){
+                if(arr[i][j]==1 && row[i]==1 && col[j]==1)ans++;
+            }
+        }
+
      return ans;
     }
 }
