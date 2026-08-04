1e8eea8a9631188c6daa86975085526bf26ad2c1
Sachin Adhikari <sachinadhikari933@gmail.com>
2026-08-05 02:28:48 +0530

    Time: 2 ms (36.38%) | Memory: 47.2 MB (58.62%) - LeetSync

--- DSA-LeetCode/801-is-graph-bipartite/is-graph-bipartite.java
+++ DSA-LeetCode/801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,24 +1,25 @@
 class Solution {
-    public static boolean helper(int arr[][] , int visited[] , int color , int curr){
-        visited[curr]=color;
 
-        for(int i  =0  ; i < arr[curr].length ; i++){
-            int val=arr[curr][i];
-            if(visited[val]==-1){
-            if(!helper(arr,visited,color^1,val) )return false;
-
-            } else if(visited[val]==visited[curr])return false;
-        }
-        return true;
-
-    }
     public boolean isBipartite(int[][] graph) {
         int arr[]=new int[graph.length];
+        Queue<Integer> q = new LinkedList<>();
         int color=0;
         Arrays.fill(arr,-1);
         for(int i = 0 ; i < graph.length ; i++ ){
             if(arr[i]!=-1)continue;
-            if(!helper(graph,arr,0,i)     )return false;
+            q.offer(i);
+             arr[i]=color;
+            while(!q.isEmpty()){
+                  int prev=q.poll();
+                int size=q.size();
+                for(int  j = 0 ; j < graph[prev].length ; j++){
+                    int curr=graph[prev][j];
+                    if(-1==arr[curr]){
+                        arr[curr]=arr[prev]^1;
+                        q.offer(curr);
+                    }else if(arr[curr]==arr[prev])return false;
+                }
+            }
         }
         return true;
     }
