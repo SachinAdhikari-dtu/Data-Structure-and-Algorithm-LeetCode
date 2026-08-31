6bfb733fed895df1bd3ddb81dbc7d270b411a9c2
Sachin Adhikari <sachinadhikari933@gmail.com>
2026-08-31 06:39:53 +0530

    Time: 48 ms (53.96%) | Memory: 119.3 MB (51.90%) - LeetSync

--- DSA-LeetCode/2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ DSA-LeetCode/2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,21 +1,20 @@
 class Solution {
-    public long help(int i , long dp[][] , int arr[] , int flag){
-        if(i >= arr.length)return 0;
-        if(dp[i][flag] != 0)return dp[i][flag];
-        long val=arr[i];
-    if(flag==1){
-        val*=-1;
-    }
-        
-    
-        long a=help(i+1,dp,arr,1-flag)+val;
-        dp[i][flag]=(long)Math.max(help(i+1,dp,arr,flag),a);
-        return dp[i][flag];
+    public static long help(int arr[] , long dp[][] , int index , int sign){
+        if(index==arr.length)return 0;
+        if(dp[index][sign]!=-1)return dp[index][sign];
+        long sum=help(arr,dp,index+1,1-sign)+arr[index];
+        if(sign==1)sum-=(2*arr[index]);
+        sum=Math.max(sum,help(arr,dp,index+1,sign));
+        dp[index][sign]=sum;
+        return sum;
     }
     public long maxAlternatingSum(int[] nums) {
-        long dp[][]=new long[nums.length+1][2];
+        long dp[][]=new long[nums.length][2];
+        for(int i =0 ; i < nums.length ; i++){
+            dp[i][0]=-1;
+            dp[i][1]=-1;
+        }
+         return          help(nums,dp,0,0);
 
-
-        return help(0,dp,nums,0);
     }
 }
